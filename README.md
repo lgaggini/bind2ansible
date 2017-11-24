ee46fa66d0bb0eddbc1e2cc54b2332dd1a82b0a3
Lorenzo Gaggini <lg@lgaggini.net>
2017-11-24 16:29:15 +0100

    first version

---
+++ README.md
@@ -0,0 +1,47 @@
+# bind2ansible
+
+bind2ansible is a tool to generate ansible static inventory from bind9 zones files. It's intended to be runned on a post-merge hook (example included) on the repo which hosts the bind zones files but it could be runned also manually.
+
+## Configuration
+At the top of both scripts there are configuration settings:
+
+### bind2ansible.sh
+
+```bash
+# default ansible user
+default_user="root"
+# default custom ssh port
+custom_port=""
+# bind zones path
+zones_path="/bind/files/zones/"
+# zones filename to parse
+zones=(db.my.zone1 db.my.zone2)
+# line (records) to include
+include_filter=" A "
+# lines (vip, windows cluster, etc..) to exclude
+exclude_filter="^;"
+```
+
+### post-merge
+```bash
+# regexp to match zone files on the git repo to monitor for changes
+bind_zone_regexp="/bind/files/zones/"
+# command to run on monitored files changes, the awk part removes
+# duplicate domains based on the last level, the first zone defined
+# in bind2ansible has the precedence
+bind2ansible_cmd="/usr/local/bin/bind2ansible.sh | awk -F"." '!x[($1)]++'"
+# where to write the ansible static inventory
+ansible_inventory="/etc/ansible/hosts"
+```
+
+## Run
+### Manual Run
+```bash
+/bind2ansible.sh
+```
+
+### Post Merge Hook Run
+Copy the post-merge script on your repo in the .git/hook folder and if the settings are ok on first git pull including files matching the `bind_zone_regexp` the script will be launched.
+
+## Future plan
+A dynamic inventory script (json output) for ansible implementing the same concept
---
+++ bind2ansible.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+#
+# Author: Lorenzo Gaggini - lg@lgaggini.net
+# Script to generate ansible static inventory from bind zones
+#
+# ./bind2ansible.sh
+
+# script configuration
+# default ansible user
+default_user="root"
+# default custom ssh port
+custom_port=""
+# bind zones path
+zones_path="/bind/files/zones/"
+# zones filename to parse
+zones=(db.my.zone1 db.my.zone2)
+# line (records) to include
+include_filter=" A "
+# lines (vip, windows cluster, etc..) to exclude
+exclude_filter="^;"
+
+# global inventory variables
+echo "[all:vars]"
+echo "ansible_connection=ssh"
+echo "ansible_user=${default_user}"
+echo "ansible_ssh_common_args='-F /etc/ansible/ansible_ssh_config'"
+echo
+
+# common zones
+for zone in ${zones[@]}; do
+    clusters=$(cat ${zones_path}${zone} | grep ${include_filter} | awk '{print $1}' | sort | uniq | grep -vE ${exclude_filter} | sed 's/[^a-Z-]//g' | uniq -c | awk '($1 > 1 )' | awk '{print $2}' | sort | uniq)
+    for cluster in $clusters; do
+        prod=0
+        stg=0
+        int=0
+        indexes=$(cat ${zones_path}${zone} | awk '{print $1}' | sort | uniq | grep -vE "^;" | grep "^${cluster}" | sed 's/[^0-9]//g' | sort | uniq)
+        for index in $indexes; do
+            dec_index=$((10#${index}))
+            case 1 in
+                $((${dec_index}<80 && ${prod}==0)))
+                    echo "[${cluster}]"
+                    prod=1;;
+                $((${dec_index}>=80 && ${dec_index} < 90 && ${stg}==0)))
+                    echo "[${cluster}-stg]"
+                    stg=1;;
+                $((${dec_index}>=90 && ${int}==0)))
+                    echo "[${cluster}-int]"
+                    int=1;;
+            esac
+            echo "${cluster}${index}.${zone:3}${custom_port}"
+        done
+    done
+    echo
+done
